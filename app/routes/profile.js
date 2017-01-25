44674cb149e590e84d7d1d69d9ae7ad2e0318623
Paul Garcia <paul.anth.garcia@gmail.com>
2017-01-25 01:44:00 -0600

    cosmetic changes

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    editBasic: false,
+    editMilitary: false,
+    editContact: false,
+    editId: false,
+    editProducts: false,
     model(params) {
         return Ember.$.get("http://localhost:8080/members/"+params.member_id);
     }
